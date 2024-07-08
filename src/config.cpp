e1f89af0aba2b77d5ea8a276447028d30aedc1de
Shawn Tsai <sstsai@gmail.com>
2024-07-07 19:57:09 -0700

    set flags needs_raw_iq and has_iq_outputs for raw_zmq

--- src/config.cpp
+++ src/config.cpp
@@ -251,6 +251,7 @@ static int parse_outputs(libconfig::Setting& outs, channel_t* channel, int i, in
             channel->outputs[oo].type = O_RAW_ZMQ;
             zmq_data* pdata = (zmq_data*)(channel->outputs[oo].data);
             pdata->address = outs[o].exists("address") ? std::string((char const*)outs[o]["address"]) : std::string("tcp://*:1234");
+            channel->needs_raw_iq = channel->has_iq_outputs = 1;
         } else {
             if (parsing_mixers) {
                 cerr << "Configuration error: mixers.[" << i << "] outputs.[" << o << "]: ";
